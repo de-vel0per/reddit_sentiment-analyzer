39dc789390b14cc8f4fc2169f3e6faa25549f05d
Sajid Saleem <msajids84@hotmail.com>
2026-05-23 23:28:04 +0500

    fix user agent

--- app.py
+++ app.py
@@ -7,13 +7,15 @@ st.title("Reddit Sentiment Analyzer")
 
 
 #headers = {"User-Agent": "sentiment-analyzer/0.1"}
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"}
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+            "Accept": "application/json",
+            "Accept-Language": "en-US,en;q=0.9",}
 
 subreddit = st.text_input("Enter subreddit name")
 button = st.button("Analyze")
+url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=25&raw_json=1"
 if subreddit and button:
-    response = requests.get(f"https://www.reddit.com/r/{subreddit}/hot.json", 
-                        headers=headers)
+    response = requests.get(url, headers=headers, timeout=10)
     st.write(response.status_code)
     try:                       
         posts = response.json()["data"]["children"]
